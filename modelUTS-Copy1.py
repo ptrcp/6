42a76588220fd6658a5b6646980059e40148296a
ptrcp <116084876+ptrcp@users.noreply.github.com>
2024-04-30 14:10:14 +0700

    Update modelUTS-Copy1.py

--- modelUTS-Copy1.py
+++ modelUTS-Copy1.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib
 import numpy as np
 
-model = joblib.load('OOPmodel2.pkl')
+model = joblib.load('OOPmodel3.pkl')
 
 def main():
     st.title(':red[Customer Churn Prediction]')
